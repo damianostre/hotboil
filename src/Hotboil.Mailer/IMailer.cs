3b059d5c56a1afb8727f7242ab890e945045c3cf
Damian Rykowski <rykowski.d@gmail.com>
2024-03-05 23:41:19 +0100

    feat: email module

---
+++ src/Hotboil.Mailer/IMailer.cs
@@ -0,0 +1,14 @@
+﻿namespace Hotboil.Mailer;
+
+public class Mailer : IMailer
+{
+    public Task SendEmailAsync(IMail mail)
+    {
+        throw new NotImplementedException();
+    }
+} 
+
+public interface IMailer
+{
+    
+}
